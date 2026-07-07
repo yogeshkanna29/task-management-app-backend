b2c5a363f635f6b343e3a3f2a5491f49741470f6
yogesh <yogeshdev2906@gmail.com>
2026-07-07 20:45:55 +0530

    main js files

---
+++ src/app.js
@@ -0,0 +1,47 @@
+/** Default imports */
+
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const { swaggerUi, swaggerSpec } = require("./swagger/swagger");
+
+/** Route imports */
+
+const authRoutes = require("./routes/auth.routes");
+const taskRoutes = require("./routes/task.routes");
+
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Swagger
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec)
+);
+
+// Home
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Task Manager Home API Running",
+  });
+});
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes); // Invoking Auth Routes
+
+app.use("/api/tasks", taskRoutes); // Invoking Task Routes
+
+
+module.exports = app;
